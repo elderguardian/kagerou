43acd9c49c5a50671d16c8141c603dbf69580b9a
elderguardian <katzenmaedchen@tutanota.com>
2023-08-17 16:03:57 +0200

    added about section component to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
     import LandingSection from "./sections/LandingSection.svelte";
     import DiscoverSection from "./sections/Discover/DiscoverSection.svelte";
+    import AboutSection from "./sections/About/AboutSection.svelte";
 </script>
 
 <header>
@@ -9,37 +10,8 @@
 </header>
 <main>
     <LandingSection></LandingSection>
+    <AboutSection></AboutSection>
     <DiscoverSection></DiscoverSection>
-    <section id="section-discover">
-        <h2 class="text-blue-to-red">Discover Awesome Creations</h2>
-        <div>
-            <article>
-                <h3>img2minecraft</h3>
-                <p>Convert image files into Minecraft blocks, maintaining the original color and look of the
-                    picture.</p>
-                <a target="_blank" href="https://github.com/elderguardian/img2minecraft">repository</a>
-                <a target="_blank" href="https://img2minecraft.kagerou.dev/">instance</a>
-            </article>
-            <article>
-                <h3>monkeyshot</h3>
-                <p>Server software for uploading images and displaying them with a pretty embed. Based on the Strawberry
-                    Framework.</p>
-                <a target="_blank" href="https://github.com/elderguardian/monkeyshot">repository</a>
-            </article>
-            <article>
-                <h3>flaggen</h3>
-                <p>A website for generating profile pictures which include country flags</p>
-                <a target="_blank" href="https://github.com/elderguardian/flaggen">repository</a>
-                <a target="_blank" href="https://flaggen.kagerou.dev/">instance</a>
-            </article>
-            <article>
-                <h3>ton</h3>
-                <p>A lightweight and classless CSS Framework, created to make creating static websites easier. </p>
-                <a target="_blank" href="https://github.com/elderguardian/ton">repository</a>
-                <a target="_blank" href="https://ton.kagerou.dev/">preview</a>
-            </article>
-        </div>
-    </section>
 </main>
 <footer>
     Copyright &copy; 2023 kagerou. All rights reserved.
