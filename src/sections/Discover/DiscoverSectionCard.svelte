2761719c533f607661afb01090fa75732444a686
elderguardian <katzenmaedchen@tutanota.com>
2023-09-19 17:20:59 +0200

    created component for discover section card

---
+++ src/sections/Discover/DiscoverSectionCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title;
+    export let description;
+    export let links = []
+</script>
+<article>
+    <h3>{title}</h3>
+    <p>{description}</p>
+    {#each links as linkItem}
+        <a target="_blank" href="{linkItem.url}">{linkItem.name}</a>
+    {/each}
+</article>
