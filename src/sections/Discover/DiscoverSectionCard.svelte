9b8e9ad6bc400c7a1446266d7c357079ef9879c0
elderguardian <katzenmaedchen@tutanota.com>
2023-09-19 17:21:19 +0200

    styled discover section card

--- src/sections/Discover/DiscoverSectionCard.svelte
+++ src/sections/Discover/DiscoverSectionCard.svelte
@@ -10,3 +10,44 @@
         <a target="_blank" href="{linkItem.url}">{linkItem.name}</a>
     {/each}
 </article>
+<style>
+
+    article {
+        text-decoration: none;
+        color: #c2c2c2;
+        width: 25%;
+        border-radius: .5vw;
+        padding: 1vw;
+    }
+
+    article > * {
+        display: block;
+        width: fit-content;
+        margin: 1vw 0;
+    }
+
+    article p {
+        font-size: 1.6vw;
+        color: #7d858f;
+        margin-bottom: 3vw;
+    }
+
+    article a {
+        font-size: 1.7vw;
+        color: #388bfd;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+
+    @media screen and (max-width: 600px) {
+        article {
+            width: 80%;
+        }
+
+        article p,
+        article a {
+            font-size: 3.5vw;
+        }
+    }
+</style>
