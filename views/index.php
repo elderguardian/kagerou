8a42d72eccbab7ebadf99dc204054fb1dcfcb54d
elderguardian <katzenmaedchen@tutanota.com>
2023-11-10 23:49:17 +0100

    basic site content

---
+++ views/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<main>
+    <section>
+        <h2>About Me</h2>
+        <p>Hey, I am a hobby developer and a computer science student at a German Fachoberschule. I create open-source software using TypeScript on my public GitHub account. You can find me as @elderguardian, like the boss from Minecraft.</p>
+    </section>
+    <section>
+        <h2>Interests</h2>
+        <ul>
+            <li>Clean code, architecture, and software design</li>
+            <li>Linux-based operating systems</li>
+            <li>Guitar / music (Tarrega, Chopin, Liszt…)</li>
+        </ul>
+        <p>You can view this page’s repository on <a href="https://github.com/elderguardian/your-repo">GitHub</a>.</p>
+    </section>
+</main>
+</body>
+</html>
