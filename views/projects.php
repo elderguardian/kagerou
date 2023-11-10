017007c00abb2069403af729f4001d43e840d423
elderguardian <katzenmaedchen@tutanota.com>
2023-11-10 23:49:32 +0100

    created projects view

---
+++ views/projects.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<main>
+      <h2>Pinned:</h2>
+      <ul>
+        <li><a href="https://github.com/elderguardian/leonie">leonie</a>: A lightweight TypeScript Discord bot.</li>
+        <li><a href="https://github.com/elderguardian/maria">maria</a>: A library to fetch information from schools in Hessen, Germany.</li>
+        <li><a href="https://github.com/elderguardian/img2minecraft">img2minecraft</a>: A web-based tool to convert images.</li>
+        <li><a href="https://github.com/elderguardian/monkeyshot">monkeyshot</a>: Image upload server software.</li>
+      </ul>
+
+      <h2>Packages:</h2>
+      <ul>
+        <li>(Docker) <a href="https://ghcr.io/elderguardian/leonie">ghcr.io/elderguardian/leonie</a></li>
+        <li>(Docker) <a href="https://ghcr.io/elderguardian/img2minecraft">ghcr.io/elderguardian/img2minecraft</a></li>
+        <li>(Docker) <a href="https://ghcr.io/elderguardian/monkeyshot">ghcr.io/elderguardian/monkeyshot</a></li>
+        <li>(NPM) <a href="https://www.npmjs.com/package/maria-hessen">npmjs.com/package/maria-hessen</a></li>
+      </ul>
+
+      <h2>Other Interesting Projects:</h2>
+      <ul>
+        <li>My GitHub contains an <a href="https://github.com/elderguardian/elderguardian">image</a>that automatically changes daily.</li>
+      </ul>
+    </main>
+  </body>
+</html>
