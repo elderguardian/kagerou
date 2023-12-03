e3278af0f12ab1a7eeae6c1904cb396af074966d
elderguardian <katzenmaedchen@tutanota.com>
2023-12-03 01:41:30 +0100

    fixes

--- views/index.php
+++ views/index.php
@@ -6,14 +6,11 @@
 <main>
     <section>
         <h2>About Me</h2>
-
         <ul>
             <li>&#129489;&#127995;&#8205;&#128187; Hobby Developer</li>
             <li>&#127465;&#127466; German Fachoberschule Student</li>
             <li>&#128100; GitHub <a href="https://github.com/elderguardian">@elderguardian</a></li>
         </ul>
-        <p>
-        </p>
     </section>
     <section>
         <h2>Interests</h2>
